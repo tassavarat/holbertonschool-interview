478a09c0c307415d821f3461a5580c15f48bd486
tassavarat <tassavarat@gmail.com>
2020-02-04 01:07:33 -0800

    adding documentation and input validation

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,13 +2,25 @@
 
 
 def canUnlockAll(boxes):
-    u = [False] * len(boxes)
+    """Checks if all boxes can be opened
+
+    Args:
+        boxes: list of lists containing boxes and their keys
+
+    Returns:
+        True if all boxes can be opened, otherwise False
+    """
+    bl = len(boxes)
+
+    if bl == 0:
+        return True
+    u = [False] * bl
     u[0] = True
     s = [0]
 
     while s:
         for k in boxes[s.pop()]:
-            if not u[k]:
+            if k > 0 and k < bl and not u[k]:
                 u[k] = True
                 s.append(k)
 
