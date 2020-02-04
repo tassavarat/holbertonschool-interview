0f9668e7d9790964e3f35713e20a6706ea061464
Tim Assavarat <tassavarat@gmail.com>
2020-02-03 19:02:20 -0800

    checking for list and integer values

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,8 +7,13 @@ def canUnlockAll(boxes):
 
     if isinstance(boxes, list):
         for i in range(len(boxes)):
+            if not isinstance(boxes[a[i]], list):
+                return False
             a.extend(boxes[a[i]])
         s.update(a)
+        for n in s:
+            if n < 0 or n > len(boxes) - 1:
+                return False
         if len(s) == len(boxes):
             return True
 
