611ae33dcd75a3040d768971cf2b85c72e184b6c
tassavarat <tassavarat@gmail.com>
2020-02-04 00:43:55 -0800

    reworked to use stack

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,25 +2,14 @@
 
 
 def canUnlockAll(boxes):
-    s = set()
-    a = [0]
+    u = [False] * len(boxes)
+    u[0] = True
+    s = [0]
 
-    if len(boxes) == 0:
-        return True
-    if isinstance(boxes, list):
-        for b in boxes:
-            if not isinstance(b, list):
-                return False
-            for ki in range(len(b)):
-                if not isinstance(b[ki], int):
-                    return False
-                if b[ki] < 0 or b[ki] > len(boxes) - 1:
-                    del b[ki]
+    while s:
+        for k in boxes[s.pop()]:
+            if not u[k]:
+                u[k] = True
+                s.append(k)
 
-        for i in range(len(boxes)):
-            a.extend(boxes[a[i]])
-        s.update(a)
-        if len(s) == len(boxes):
-            return True
-
-    return False
+    return all(u)
