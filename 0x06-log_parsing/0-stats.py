4fa342bbbda8657d8a73bce34cf2375061414cc9
tassavarat <tassavarat@gmail.com>
2020-03-18 22:38:12 -0700

    Handling int conversion failure and only printing k, v pairs if v non zero

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,38 +1,36 @@
 #!/usr/bin/python3
 from sys import stdin
-from collections import OrderedDict
 
 fs = -1
 sc = -2
 
+if __name__ == "__main__":
+    def printstat():
+        print("File size:", file_size)
+        for k, v in stat.items():
+            if v:
+                print("{}: {}".format(k, v))
 
-def printstat():
-    print("File size:", file_size)
-    for k, v in stat.items():
-        print("{}: {}".format(k, v))
+    def parselog(file_size):
+        arr = line.split()
+        try:
+            file_size += int(arr[fs])
+            if arr[sc] in stat:
+                stat[arr[sc]] += 1
+        except Exception as e:
+            print(e)
+        return file_size
 
-
-def parselog(file_size):
-    arr = line.split()
-    # print("debug", arr[sc])
-    file_size += int(arr[fs])
-    if arr[sc] in stat:
-        stat[arr[sc]] += 1
-        # print(stat.items())
-    return file_size
-
-
-file_size = 0
-count = 1
-stat = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0,
-        "500": 0}
-try:
-    for line in stdin:
-        file_size = parselog(file_size)
-        if count % 10 == 0:
-            # print("10 lines")
-            printstat()
-        count += 1
-except KeyboardInterrupt:
-    printstat()
-    raise
+    try:
+        file_size = 0
+        count = 1
+        stat = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
+                "405": 0, "500": 0}
+        for line in stdin:
+            file_size = parselog(file_size)
+            if count % 10 == 0:
+                printstat()
+            count += 1
+    except KeyboardInterrupt:
+        printstat()
+        raise
