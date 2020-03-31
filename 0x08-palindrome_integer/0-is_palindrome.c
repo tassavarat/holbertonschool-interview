72b0b9a4b58893d2997c132f0650510f9de0b4e8
tassavarat <tassavarat@gmail.com>
2020-03-31 16:44:59 -0700

    Altering buffer size

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,3 +1,40 @@
+/* #include "palindrome.h" */
+
+/* size_t intlen(size_t n) */
+/* { */
+/* 	size_t len; */
+
+/* 	while (n) */
+/* 	{ */
+/* 		n /= 10; */
+/* 		++len; */
+/* 	} */
+/* 	return (len); */
+/* } */
+
+/**
+ * is_palindrome - checks whether given unsigned integer is a palindrome
+ * @n: number to check
+ *
+ * Return: 1 if palindrome, otherwise 0
+ */
+/* int is_palindrome(unsigned long n) */
+/* { */
+/* 	size_t len, beg, end, i; */
+
+/* 	len = i = intlen(n); */
+/* 	for (beg = 1; --i; beg *= 10) */
+/* 		; */
+/* 	end = 1; */
+/* 	for (i = 0; i < len / 2; ++i) */
+/* 	{ */
+/* 		if (n / beg % 10 != n / end % 10) */
+/* 			return (0); */
+/* 		beg /= 10; */
+/* 		end *= 10; */
+/* 	} */
+/* 	return (1); */
+/* } */
 #include "palindrome.h"
 
 /**
@@ -8,7 +45,7 @@
  */
 int is_palindrome(unsigned long n)
 {
-	char str[128];
+	char str[20];
 	size_t i, end;
 
 	sprintf(str, "%lu", n);
