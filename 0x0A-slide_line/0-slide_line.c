48b1b326449353abaadd50a4ed7b0a45c2874b3e
tassavarat <tassavarat@gmail.com>
2020-04-15 19:30:54 -0700

    Using functions instead of macros

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 
 /**
+ * combine - combines two values together
+ * @line: pointer to array of integers
+ * @i: pointer to line index
+ * @direction: direction to merge values
+ *
+ * Description: modifies i depending on direction, this is done for the calling
+ * function
+ */
+void combine(int *line, size_t *i, int direction)
+{
+	if (line[*i] == line[*i - direction])
+	{
+		line[*i] += line[*i - direction];
+		line[*i - direction] ^= line[*i - direction];
+		*i -= direction;
+	}
+}
+/**
  * merge - merges array adjacent identical array values in specified direction
  * @line: pointer to array of integers
  * @size: size of array
@@ -13,10 +31,36 @@ void merge(int *line, size_t size, int direction)
 
 	if (direction == SLIDE_LEFT)
 		for (i = 0; line[i] && i < size; ++i)
-			MERGE(line, i, direction);
+			combine(line, &i, direction);
 	else
 		for (i = size - 1; line[i] && (int) i > -1; --i)
-			MERGE(line, i, direction);
+			combine(line, &i, direction);
+}
+
+/**
+ * swap - swaps two values in an array
+ * @line: pointer to array of integers
+ * @direction: direction to slide values
+ * @empt: pointer to line index with 0 value
+ * @i: line index used to check for non 0 values
+ * @merged: specifies if merging has been done
+ *
+ * Description: modifies empt depending on direction, this is done
+ * for the calling function
+ * Return: 0 initially, 1 subsequently, returning 1 dependent on merged value
+ */
+int swap(int *line, int direction, size_t *empt, size_t i, int merged)
+{
+	if (merged && !line[i] && !line[i - direction])
+		return (1);
+	if (line[i])
+	{
+		line[*empt] ^= line[i];
+		line[i] ^= line[*empt];
+		line[*empt] ^= line[i];
+		*empt -= direction;
+	}
+	return (0);
 }
 
 /**
@@ -27,16 +71,26 @@ void merge(int *line, size_t size, int direction)
  */
 void slide(int *line, size_t size, int direction)
 {
+	static int merged;
 	size_t i, empt;
 
 	if (direction == SLIDE_LEFT)
-		for (empt = i = 0; empt < size - 1 && i < size; ++empt)
-			for (i = empt + 1; !line[empt] && i < size; ++i)
-				SWAP(line, empt, i);
+	{
+		for (empt = 0; line[empt] && empt < size; ++empt)
+			;
+		for (i = empt + 1; i < size; ++i)
+			if (swap(line, direction, &empt, i, merged))
+				break;
+	}
 	else
-		for (empt = i = size - 1; empt > 0 && (int) i > -1; --empt)
-			for (i = empt - 1; !line[empt] && (int) i > -1; --i)
-				SWAP(line, empt, i);
+	{
+		for (empt = size - 1; line[empt] && (int) empt > -1; --empt)
+			;
+		for (i = empt - 1; (int) i > -1; --i)
+			if (swap(line, direction, &empt, i, merged))
+				break;
+	}
+	merged = 1;
 }
 
 /**
