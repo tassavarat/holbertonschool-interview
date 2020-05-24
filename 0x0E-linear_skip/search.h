35b3a4caeea53d46a643b66c7a33032fbcfb1c32
tassavarat <tassavarat@gmail.com>
2020-05-24 15:13:04 -0700

    Formatting changes

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -17,10 +17,10 @@
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
