b39a33dd4ed00dd0a148e2538001f63a33dc5665
tassavarat <tassavarat@gmail.com>
2020-06-10 19:42:06 -0700

    Rain water solution

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+def rain(walls):
+    """Compute how much water can be trapped
+
+    Args:
+        walls: list of height values
+
+    Returns:
+        amount of water able to be trapped
+    """
+    l = water = lmax = rmax = 0
+    r = len(walls) - 1
+
+    while l < r:
+        if walls[l] < walls[r]:
+            if walls[l] < lmax:
+                water += lmax - walls[l]
+            else:
+                lmax = walls[l]
+            l += 1
+        else:
+            if walls[r] < rmax:
+                water += rmax - walls[r]
+            else:
+                rmax = walls[r]
+            r += 1
+    return water
