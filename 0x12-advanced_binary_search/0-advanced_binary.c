4847c3cc094602fde999f664116c0a6fb1345281
tassavarat <tassavarat@gmail.com>
2020-06-23 17:29:04 -0700

    Separating print into another function and handling size being 0

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,6 +1,22 @@
 #include "search_algos.h"
 
 /**
+ * print_search - print part of array being searched
+ * @a: array being searched
+ * @l: left boundary index
+ * @r: right boundary index
+ */
+void print_search(int *a, int l, int r)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = l; i <= r; ++i)
+		printf("%d%s", a[i], i < r ? ", " : "");
+	printf("\n");
+}
+
+/**
  * binary_search - recursive binary search algorithm
  * @a: pointer to array to search
  * @target: target to find
@@ -15,10 +31,7 @@ int binary_search(int *a, int target, int l, int r)
 
 	if (l == r)
 		return (l);
-	printf("Searching in array: ");
-	for (i = l; i <= r; ++i)
-		printf("%d%s", a[i], i < r ? ", " : "");
-	printf("\n");
+	print_search(a, l, r);
 	i = (l + r) / 2;
 	if (a[i] < target)
 		l = i + 1;
@@ -35,11 +48,11 @@ int binary_search(int *a, int target, int l, int r)
  * @value: value to search for
  *
  * Return: index where value is located
- * -1 if array is NULL or value not found
+ * -1 if array is NULL, size is 0, or value not found
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (!array)
+	if (!array || !size)
 		return (-1);
 	return (binary_search(array, value, 0, size - 1));
 }
