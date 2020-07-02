f8df21f918b210b985d7e5ed7d4bb3b0a62ab95a
tassavarat <tassavarat@gmail.com>
2020-07-01 22:26:07 -0700

    Properly sorting

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -5,7 +5,7 @@ import collections
 import requests
 
 
-def fill_list(subreddit, hot_list=[], after=""):
+def fill_list(subreddit, hot_list=[], after=None):
     """Create list of words in hot titles of specified subreddit
 
     Args:
@@ -15,17 +15,19 @@ def fill_list(subreddit, hot_list=[], after=""):
 
     Returns: populated list or None of failure
     """
-    req = requests.get("https://reddit.com/r/{}/hot.json?after={}".
+    req = requests.get("https://www.reddit.com/r/{}/hot.json?after={}".
                        format(subreddit, after),
-                       headers={"User-agent": "agent"})
+                       headers={"User-agent": "agent"},
+                       allow_redirects=False)
     if req.status_code != 200:
         return None
+    after = req.json().get("data").get("after")
     if after is None:
         return hot_list
     for i in req.json().get("data").get("children"):
         for word in i.get("data").get("title").split():
             hot_list.append(word.lower())
-    return fill_list(subreddit, hot_list, req.json().get("data").get("after"))
+    return fill_list(subreddit, hot_list, after)
 
 
 def count_words(subreddit, word_list):
@@ -41,14 +43,18 @@ def count_words(subreddit, word_list):
     Returns: OrderedDict with keys as keywords and occurrences as values or
     None on request failure
     """
+    if subreddit is None or subreddit == "" or word_list is None:
+        return None
     hot_list = fill_list(subreddit)
     if hot_list is None:
         return None
     all_cnt = collections.Counter(hot_list)
-    filtered_cnt = collections.OrderedDict()
+    filtered_cnt = {}
     for word in word_list:
         word_l = word.lower()
-        if all_cnt[word_l] > 0:
-            print("{}: {}".format(word, all_cnt[word_l]))
+        if all_cnt[word_l] > 0 and word_l not in filtered_cnt:
             filtered_cnt[word_l] = all_cnt[word_l]
+    for k, v in sorted(filtered_cnt.items(),
+                       key=lambda item: item[1], reverse=True):
+        print("{}: {}".format(k, v))
     return filtered_cnt
