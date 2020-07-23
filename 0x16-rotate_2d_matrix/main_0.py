00e26a1f38e609e7dfe22877803156c41cb47613
tassavarat <tassavarat@gmail.com>
2020-07-22 23:21:03 -0700

    Reworked logic

--- 0x16-rotate_2d_matrix/main_0.py
+++ 0x16-rotate_2d_matrix/main_0.py
@@ -8,6 +8,10 @@ if __name__ == "__main__":
     matrix = [[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]]
+    # matrix = [[1, 2, 3, 4],
+    #           [5, 6, 7, 8],
+    #           [9, 10, 11, 12],
+    #           [13, 14, 15, 16]]
 
     rotate_2d_matrix(matrix)
     print(matrix)
