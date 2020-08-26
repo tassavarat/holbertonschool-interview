989e7e6960d7f053c07c85a016a1383e47cf9963
tassavarat <tassavarat@gmail.com>
2020-08-26 00:46:03 -0700

    Not validating coins

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -11,7 +11,7 @@ def makeChange(coins, total):
 
     Returns: minimum number of coins needed to reach total
     """
-    if not coins or total < 1:
+    if total < 1:
         return 0
     min_arr = [float("inf")] * (total + 1)
     min_arr[0] = 0
