7cecfe11e1485f78ebd1d102ac292a0ec5c4bdfa
tassavarat <tassavarat@gmail.com>
2020-09-02 19:56:26 -0700

    Implementing double circular linked list insertion

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,73 @@
+#include "list.h"
+
+/**
+ * create_node - create linked list node
+ * @str: string to initialise list data to
+ *
+ * Return: created node
+ */
+List *create_node(char *str)
+{
+	List *new;
+
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	if (!new->str)
+		return (NULL);
+	return (new);
+}
+
+/**
+ * add_node_begin - insert new node to beginning of double circular linked list
+ * @list: linked list being inserted to
+ * @str: string to initialise list data to
+ *
+ * Return: pointer to new node, NULL on error
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new;
+
+	new = create_node(str);
+	if (!new)
+		return (NULL);
+	*list = new;
+	if (!*list)
+	{
+		(*list)->next = (*list)->prev = new;
+	}
+	else
+	{
+		new->prev = (*list)->prev, new->next = *list;
+		(*list)->prev = (*list)->prev->next = new;
+	}
+	return (new);
+}
+
+/**
+ * add_node_end - insert new node to end of double circular linked list
+ * @list: linked list being inserted to
+ * @str: string to initialise list data to
+ *
+ * Return: pointer to new node, NULL on error
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new;
+
+	new = create_node(str);
+	if (!new)
+		return (NULL);
+	if (!*list)
+	{
+		*list = new, (*list)->next = (*list)->prev = new;
+	}
+	else
+	{
+		new->prev = (*list)->prev, new->next = *list;
+		(*list)->prev = (*list)->prev->next = new;
+	}
+	return (new);
+}
