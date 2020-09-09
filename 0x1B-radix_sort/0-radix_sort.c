8d2ed585ed96fd7b455d3353f92f3bab458215a5
tassavarat <tassavarat@gmail.com>
2020-09-08 18:51:36 -0700

    Renamed place to sig_dig

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -4,23 +4,23 @@
  * csort_radix - counting sort to be called by radix_sort
  * @array: array to sort
  * @size: size of array
- * @place: current digit place to sort
+ * @sig_dig: current significant digit to sort
  * @sorted: array to store sorted values in
  */
-void csort_radix(int *array, size_t size, int place, int *sorted)
+void csort_radix(int *array, size_t size, int sig_dig, int *sorted)
 {
 	int count[BASE] = {0};
 	size_t i;
 	ssize_t j;
 
 	for (i = 0; i < size; ++i)
-		++count[array[i] / place % 10];
+		++count[array[i] / sig_dig % 10];
 	for (i = 1; i < BASE; ++i)
 		count[i] += count[i - 1];
 	for (j = size - 1; j >= 0; --j)
 	{
-		sorted[count[array[j] / place % 10] - 1] = array[j];
-		--count[array[j] / place % 10];
+		sorted[count[array[j] / sig_dig % 10] - 1] = array[j];
+		--count[array[j] / sig_dig % 10];
 	}
 	memcpy(array, sorted, size * sizeof(*sorted));
 }
@@ -50,7 +50,7 @@ int find_max(int *array, size_t size)
  */
 void radix_sort(int *array, size_t size)
 {
-	int max, place, *sorted;
+	int max, sig_dig, *sorted;
 
 	if (!array || size < 2)
 		return;
@@ -58,9 +58,9 @@ void radix_sort(int *array, size_t size)
 	if (!sorted)
 		return;
 	max = find_max(array, size);
-	for (place = 1; max / place > 0; place *= BASE)
+	for (sig_dig = 1; max / sig_dig > 0; sig_dig *= BASE)
 	{
-		csort_radix(array, size, place, sorted);
+		csort_radix(array, size, sig_dig, sorted);
 		print_array(array, size);
 	}
 	free(sorted);
