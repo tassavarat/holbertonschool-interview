22e647451a2ad973961e189e784f0f72cbc0798f
tassavarat <tassavarat@gmail.com>
2020-09-23 22:39:35 -0700

    Implementing AVL tree check

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+#include <stdio.h>
+
+int avl_check(const binary_tree_t *node, int min, int max, int *height)
+{
+	int height_l, height_r;
+
+	height_r = height_l = 0;
+	if (!node)
+		return (1);
+	if (node->n <= min || node->n >= max)
+		return (0);
+	if (!avl_check(node->left, min, node->n, &height_l) ||
+			!avl_check(node->right, node->n, max, &height_r))
+		return (0);
+	*height = (height_l > height_r ? height_l : height_r) + 1;
+	return (abs(height_l - height_r) < 2);
+}
+
+/**
+ * binary_tree_is_avl - check if binary tree is a valid AVL tree
+ * @tree: pointer to root node of tree to check
+ *
+ * Return: 1 if valid AVL tree, 0 if not
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int height = 0;
+
+	if (!tree)
+		return (0);
+	return (avl_check(tree, INT_MIN, INT_MAX, &height));
+}
