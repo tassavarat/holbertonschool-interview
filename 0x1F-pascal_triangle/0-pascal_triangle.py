e1314e2c2fd56c39e2f7fa22fa9c3bf14bf3ede5
tassavarat <tassavarat@gmail.com>
2020-10-08 00:13:21 -0700

    Implementing Pascal's triangle

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    """Create Pascal's triangle of n height
+
+    Args:
+        n: height of triangle
+
+    Returns: created triangle
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        sub = [1] * (i + 1)
+        for j in range(i - 1):
+            sub[j + 1] = triangle[i - 1][j] + triangle[i - 1][j + 1]
+        triangle.append(sub)
+    return triangle
